da81705ce9d90fc83772f39d8c1ed319bb0c9a76
Maksym Yakovlev <maksym.yakovlev@wikrgroup.com>
2023-02-17 13:10:02 +0200

    [WF-I-1195] updates `Package.swift`.

--- Package.swift
+++ Package.swift
@@ -13,10 +13,12 @@ let package = Package(
   products: [
     .library(
       name: "ComposableArchitecture",
+      type: .dynamic,
       targets: ["ComposableArchitecture"]
     ),
     .library(
       name: "Dependencies",
+      type: .dynamic,
       targets: ["Dependencies"]
     ),
   ],
